bff7c627755f6a48fb1ebbaf2b0fc11f720482fb
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-12-05 16:18:37 +0530

    Non Blocking way with CallBack HELL Method

--- index.js
+++ index.js
@@ -1,8 +1,27 @@
 const fs = require('fs');
 
-const textInput = fs.readFileSync('./txt/input.txt', 'utf-8');
+// Blocking synchronous way
 
-const textOutput = `This is what we know about the avocado: ${textInput}.\nCreated on ${Date.now()}`;
+// const textInput = fs.readFileSync('./txt/input.txt', 'utf-8');
 
-fs.writeFileSync('./txt/output.txt', textOutput);
-console.log('File written!');
+// const textOutput = `This is what we know about the avocado: ${textInput}.\nCreated on ${Date.now()}`;
+
+// fs.writeFileSync('./txt/output.txt', textOutput);
+// console.log('File written!');
+
+// Non-blocking asynchronous way
+
+fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+  fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+    console.log(data2);
+    fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+      console.log(data3);
+
+      fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
+        console.log('Your file has been written!');
+      });
+    });
+  });
+});
+
+console.log('Will read file!');
