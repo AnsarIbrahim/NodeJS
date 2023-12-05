afcc0ec441a94fabbd18b20577bfd972620d365f
Ansar Ibrahim <117971223+AnsarIbrahim@users.noreply.github.com>
2023-12-05 16:20:00 +0530

    Non Blocking way with CallBack HELL Method

--- index.js
+++ index.js
@@ -12,12 +12,16 @@ const fs = require('fs');
 // Non-blocking asynchronous way
 
 fs.readFile('./txt/start.txt', 'utf-8', (err, data1) => {
+  if (err) return console.log('ERROR! 💥');
   fs.readFile(`./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+    if (err) return console.log('ERROR! 💥');
     console.log(data2);
     fs.readFile('./txt/append.txt', 'utf-8', (err, data3) => {
+      if (err) return console.log('ERROR! 💥');
       console.log(data3);
 
       fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', (err) => {
+        if (err) return console.log('ERROR! 💥');
         console.log('Your file has been written!');
       });
     });
